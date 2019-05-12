e1baac821a707b24ff174da91da656f29162bf7e
Oliveer Farnworth <oliver@turf.org>
2019-05-12 11:57:01 +0100

    Initial Commit

---
+++ ARMagic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARMagic
+//
+//  Created by Oliver Farnworth on 12/05/2019.
+//  Copyright © 2019 Oliver Farnworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
